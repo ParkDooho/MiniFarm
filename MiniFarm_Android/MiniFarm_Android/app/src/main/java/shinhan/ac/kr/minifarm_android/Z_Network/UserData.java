6af2501ff907a2c9228512a72eec43b9ef38021f
ParkDooho <pdh2909@naver.com>
2020-03-12 13:05:17 +0900

    Create UserData.java

---
+++ MiniFarm_Android/MiniFarm_Android/app/src/main/java/shinhan/ac/kr/minifarm_android/Z_Network/UserData.java
@@ -0,0 +1,15 @@
+package shinhan.ac.kr.minifarm_android.Z_Network;
+
+public class UserData {
+    static String Id;
+
+    public static void setUserId(String userid) {
+        Id = userid;
+
+    }
+    public static String getUserId(){
+        return Id;
+    }
+
+
+}
