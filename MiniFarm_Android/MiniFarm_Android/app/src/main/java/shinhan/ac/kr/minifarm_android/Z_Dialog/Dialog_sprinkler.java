1a4137922010c68893f4626919e7a249a455e7b8
ParkDooho <pdh2909@naver.com>
2020-03-12 13:04:58 +0900

    Create Dialog_sprinkler.java

---
+++ MiniFarm_Android/MiniFarm_Android/app/src/main/java/shinhan/ac/kr/minifarm_android/Z_Dialog/Dialog_sprinkler.java
@@ -0,0 +1,21 @@
+package shinhan.ac.kr.minifarm_android.Z_Dialog;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+import shinhan.ac.kr.minifarm_android.R;
+
+public class Dialog_sprinkler extends Dialog {
+
+    public Dialog_sprinkler(Context context){
+        super(context);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        setContentView(R.layout.activity_dialog_sprinkler);
+    }
+}
