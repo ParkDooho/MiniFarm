0da2661969224a4ea7b5df91fc1144bf983d884d
ParkDooho <pdh2909@naver.com>
2020-03-12 13:04:30 +0900

    Create SearchDTO.java

---
+++ MiniFarm_Android/MiniFarm_Android/app/src/main/java/shinhan/ac/kr/minifarm_android/Z_Data/SearchDTO.java
@@ -0,0 +1,96 @@
+package shinhan.ac.kr.minifarm_android.Z_Data;
+
+public class SearchDTO {
+    private String userId;
+    private String searchName;
+    private float searchTem;
+    private float searchHum;
+    private int searchIllu;
+    private float searchPH;
+    private int searchFD;
+
+    public SearchDTO() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+
+
+    public SearchDTO(String userId, String searchName, float searchTem, float searchHum, int searchIllu, float searchPH,
+                     int searchFD) {
+        super();
+        this.userId = userId;
+        this.searchName = searchName;
+        this.searchTem = searchTem;
+        this.searchHum = searchHum;
+        this.searchIllu = searchIllu;
+        this.searchPH = searchPH;
+        this.searchFD = searchFD;
+    }
+
+
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getSearchName() {
+        return searchName;
+    }
+
+    public void setSearchName(String searchName) {
+        this.searchName = searchName;
+    }
+
+    public float getSearchTem() {
+        return searchTem;
+    }
+
+    public void setSearchTem(float searchTem) {
+        this.searchTem = searchTem;
+    }
+
+    public float getSearchHum() {
+        return searchHum;
+    }
+
+    public void setSearchHum(float searchHum) {
+        this.searchHum = searchHum;
+    }
+
+    public int getSearchIllu() {
+        return searchIllu;
+    }
+
+    public void setSearchIllu(int searchIllu) {
+        this.searchIllu = searchIllu;
+    }
+
+    public float getSearchPH() {
+        return searchPH;
+    }
+
+    public void setSearchPH(float searchPH) {
+        this.searchPH = searchPH;
+    }
+
+    public int getSearchFD() {
+        return searchFD;
+    }
+
+    public void setSearchFD(int searchFD) {
+        this.searchFD = searchFD;
+    }
+
+    @Override
+    public String toString() {
+        return "SearchDTO [userId=" + userId + ", searchName=" + searchName + ", searchTem=" + searchTem
+                + ", searchHum=" + searchHum + ", searchIllu=" + searchIllu + ", searchPH=" + searchPH + ", searchFD="
+                + searchFD + "]";
+    }
+
+}
