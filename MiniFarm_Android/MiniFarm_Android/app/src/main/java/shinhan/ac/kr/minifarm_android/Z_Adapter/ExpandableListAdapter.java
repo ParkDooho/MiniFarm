d2f420821624bcf164df9ba0de6776bbb73a6076
ParkDooho <pdh2909@naver.com>
2020-03-12 13:03:55 +0900

    Create ExpandableListAdapter.java

---
+++ MiniFarm_Android/MiniFarm_Android/app/src/main/java/shinhan/ac/kr/minifarm_android/Z_Adapter/ExpandableListAdapter.java
@@ -0,0 +1,115 @@
+package shinhan.ac.kr.minifarm_android.Z_Adapter;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+import shinhan.ac.kr.minifarm_android.R;
+
+/**
+ * Created by Hyeeun on 2017-11-28.
+ */
+
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private List<String> expandableListTitle;
+    private HashMap<String, List<String>> expandableListDetail;
+
+    //ExpandableListAdapter 생성자
+    public ExpandableListAdapter(Context context, List<String> expandableListTitle,
+                                 HashMap<String, List<String>> expandableListDetail) {
+        this.context = context;
+        this.expandableListTitle = expandableListTitle;
+        this.expandableListDetail = expandableListDetail;
+    }
+
+
+    @Override
+    public Object getChild(int listPosition, int expandedListPosition) {
+        return this.expandableListDetail.get(this.expandableListTitle.get(listPosition))
+                .get(expandedListPosition);
+    }
+
+    // ChildList의 ID로 long 형 값을 반환
+    @Override
+    public long getChildId(int listPosition, int expandedListPosition) {
+        return expandedListPosition;
+    }
+
+    @Override
+    public View getChildView(int listPosition, final int expandedListPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+        final String expandedListText = (String) getChild(listPosition, expandedListPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            //layout연결
+            convertView = layoutInflater.inflate(R.layout.list_item, null);
+        }
+        TextView expandedListTextView = (TextView) convertView.findViewById(R.id.expandedListItem);
+        expandedListTextView.setText(expandedListText);
+        return convertView;
+    }
+
+    //chillist의 크기를 int 형으로 반환
+    @Override
+    public int getChildrenCount(int listPosition) {
+        return this.expandableListDetail.get(this.expandableListTitle.get(listPosition))
+                .size();
+    }
+
+    //parenlist의 position을 받아 해당 string을 반환
+    @Override
+    public Object getGroup(int listPosition) {
+        return this.expandableListTitle.get(listPosition);
+    }
+
+    //parentlist의 원소 개수를 반환
+    @Override
+    public int getGroupCount() {
+        return this.expandableListTitle.size();
+    }
+
+    @Override
+    public long getGroupId(int listPosition) {
+        return listPosition;
+    }
+
+    @Override
+    public View getGroupView(int listPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        String listTitle = (String) getGroup(listPosition);
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            //parentlist의 layout연결
+            convertView = layoutInflater.inflate(R.layout.list_group, null);
+        }
+
+        //layout 내 TextView를 연결
+        TextView listTitleTextView = (TextView) convertView.findViewById(R.id.listTitle);
+        listTitleTextView.setTypeface(null, Typeface.BOLD);
+        listTitleTextView.setText(listTitle);
+        return convertView;
+    }
+
+    //stable ID인지 boolean 값으로 반환
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    //선택여부를 boolean값으로 반환
+    @Override
+    public boolean isChildSelectable(int listPosition, int expandedListPosition) {
+        return true;
+    }
+}
