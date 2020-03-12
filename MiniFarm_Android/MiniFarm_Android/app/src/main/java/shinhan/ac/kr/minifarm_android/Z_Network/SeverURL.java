eaf2cb14c0aa6f2650d53920cc9406f00cc4eb9f
ParkDooho <pdh2909@naver.com>
2020-03-12 13:05:16 +0900

    Create SeverURL.java

---
+++ MiniFarm_Android/MiniFarm_Android/app/src/main/java/shinhan/ac/kr/minifarm_android/Z_Network/SeverURL.java
@@ -0,0 +1,13 @@
+package shinhan.ac.kr.minifarm_android.Z_Network;
+
+public class SeverURL {
+
+
+    public static String URL() {
+        //String url="http://58.122.143.154:8080/MiniFarm_Server/MiniFarm_server.jsp";
+        String url="http://118.223.17.126:8080/MiniFarm_Server/MiniFarm_server.jsp";
+        return url;
+    }
+
+
+} 
