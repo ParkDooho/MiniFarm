e168374bc1a5aaaf6c00e318250a6991777507dd
ParkDooho <pdh2909@naver.com>
2020-03-12 13:04:18 +0900

    Create QuestionDTO.java

---
+++ MiniFarm_Android/MiniFarm_Android/app/src/main/java/shinhan/ac/kr/minifarm_android/Z_Data/QuestionDTO.java
@@ -0,0 +1,72 @@
+package shinhan.ac.kr.minifarm_android.Z_Data;
+
+import java.sql.Date;
+
+public class QuestionDTO {
+    private int qNumber;
+    private String qTitle;
+    private String userId;
+    private String qQuestion;
+    private Date qDate;
+
+    public QuestionDTO() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public QuestionDTO(int qNumber, String qTitle, String userId, String qQuestion, Date qDate) {
+        super();
+        this.qNumber = qNumber;
+        this.qTitle = qTitle;
+        this.userId = userId;
+        this.qQuestion = qQuestion;
+        this.qDate = qDate;
+    }
+
+    public int getqNumber() {
+        return qNumber;
+    }
+
+    public void setqNumber(int qNumber) {
+        this.qNumber = qNumber;
+    }
+
+    public String getqTitle() {
+        return qTitle;
+    }
+
+    public void setqTitle(String qTitle) {
+        this.qTitle = qTitle;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getqQuestion() {
+        return qQuestion;
+    }
+
+    public void setqQuestion(String qQuestion) {
+        this.qQuestion = qQuestion;
+    }
+
+    public Date getqDate() {
+        return qDate;
+    }
+
+    public void setqDate(Date qDate) {
+        this.qDate = qDate;
+    }
+
+    @Override
+    public String toString() {
+        return "QuestionDTO [qNumber=" + qNumber + ", qTitle=" + qTitle + ", userId=" + userId + ", qQuestion="
+                + qQuestion + ", qDate=" + qDate + "]";
+    }
+
+}
